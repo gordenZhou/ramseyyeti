1a9d6516532598db7fe38d48a56fbd009c288d3a
LinMac <linzhou@cs.ucsb.edu>
2014-05-23 01:16:10 -0700

    ccc

--- cyclic.c
+++ cyclic.c
@@ -181,202 +181,74 @@ int CliqueCount(int *g,
      }
     return(count);
 }
+void randomgenrow(int gsize){
+    int *r = (int *)malloc(gsize*sizeof(int));
+    int ones = gsize/2;
+    int zeros = gsize - ones;
+    int j = 0;
+    int k = 0;
+    int tmp;
+    int same;
+    int a = 1;
+    int step = 0;
+    int tt = 0;
+    FILE* logfp = fopen("../log/cycliclog.txt","a");
+    while (a == 1){
 
-/*
- ***
- *** returns the number of monochromatic cliques in the graph presented to
- *** it
- ***
- *** graph is stored in row-major order
- *** only checks values above diagonal
- */
-
-int CliqueCountFourD(int *g,
-	     int gsize,int i,int j,int precount)
-{
-    int k;
-    int l;
-    int m;
-    int n;
-    int count=0;
-    int sgsize = 4;
-    
-    int newvalue = g[i*gsize + j];
-    int prevalue = 1-newvalue;
-
-    int oldcount = 0;
-    for(k=0;k < gsize-sgsize+1; k++) 
-    {
-	if(k != i && k != j && (prevalue == g[minlin(i,k)*gsize+maxlin(i,k)]) && 
-	   	(prevalue == g[minlin(j,k)*gsize+maxlin(j,k)]))
-	{
-	    for(l=k+1;l < gsize-sgsize+2; l++) 
-	    { 
-		if(l!=i && l != j && (prevalue == g[minlin(i,l)*gsize+maxlin(i,l)]) && 
-		   (prevalue == g[minlin(j,l)*gsize+maxlin(j,l)]) && 
-		   (prevalue == g[k*gsize+l]))
-		{
-		    for(m=l+1;m < gsize-sgsize+3; m++) 
-		    {
-			if(m != i && m!=j && (prevalue == g[minlin(i,m)*gsize+maxlin(i,m)]) && 
-			   (prevalue == g[minlin(j,m)*gsize+maxlin(j,m)]) &&
-			   (prevalue == g[k*gsize+m]) && 
-			   (prevalue == g[l*gsize+m])) {
-				for(n=m+1; n < gsize-sgsize+4; n++)
-				{
-					if(n != i && n!=j && (prevalue
-						== g[minlin(i,n)*gsize+maxlin(i,n)]) &&
-					   (prevalue 
-						== g[minlin(j,n)*gsize+maxlin(j,n)]) &&
-					   (prevalue 
-						== g[k*gsize+n]) &&
-					   (prevalue 
-						== g[l*gsize+n]) &&
-					   (prevalue 
-						== g[m*gsize+n])) {
-		      					oldcount++;
-					}
-				}
-			}
-		    }
-		}
-	    }
-	}
+        r[0] = 0;
+        ones = gsize/2;
+        zeros = gsize - ones;
+        j = 1;
+        
+        while (j < gsize/2){
+            if (zeros == 0){
+                r[j] = 1;
+                r[gsize-j] = 1;
+                ones--;
+                if (j != gsize - j) ones--;
+                j++;
+                continue;
+            }else if (ones == 0){
+                r[j] = 0;
+                r[gsize-j] = 0;
+                if (j != gsize -j) zeros--;
+                j++;
+                continue;
+            }
+            
+            tmp = rand() % 2;
+            if (tmp == 0){
+                r[j] = 0;
+                r[gsize-j] = 0;
+                zeros--;
+            }else{
+                r[j] = 1;
+                r[gsize-j] = 1;
+                ones--;
+            }
+            j++;
+        }
+        for (step = 1;step < gsize/5;step++){
+            same = 1;
+            for (k=1;k<5;k++){
+                if (r[k*step] == r[(k+1)*step])
+                    same++;
+            }
+            if (same >= 5)
+                break;
+        }
+        if (same >= 5 && tt %100 == 0){
+            fprintf(logfp,"same: %d, step: %d tt: %d\n",same,step,tt);
+        }
+        tt++;
     }
-
-
-    int newcount = 0;
-    for(k=0;k < gsize-sgsize+1; k++) 
-    {
-	if(k != i && k != j && (newvalue == g[minlin(i,k)*gsize+maxlin(i,k)]) && 
-	   	(newvalue == g[minlin(j,k)*gsize+maxlin(j,k)]))
-	{
-	    for(l=k+1;l < gsize-sgsize+2; l++) 
-	    { 
-		if(l!=i && l != j && (newvalue == g[minlin(i,l)*gsize+maxlin(i,l)]) && 
-		   (newvalue == g[minlin(j,l)*gsize+maxlin(j,l)]) && 
-		   (newvalue == g[k*gsize+l]))
-		{
-		    for(m=l+1;m < gsize-sgsize+3; m++) 
-		    {
-			if(m != i && m!=j && (newvalue == g[minlin(i,m)*gsize+maxlin(i,m)]) && 
-			   (newvalue == g[minlin(j,m)*gsize+maxlin(j,m)]) &&
-			   (newvalue == g[k*gsize+m]) && 
-			   (newvalue == g[l*gsize+m])) {
-				for(n=m+1; n < gsize-sgsize+4; n++)
-				{
-					if(n != i && n!=j && (newvalue
-						== g[minlin(i,n)*gsize+maxlin(i,n)]) &&
-					   (newvalue 
-						== g[minlin(j,n)*gsize+maxlin(j,n)]) &&
-					   (newvalue 
-						== g[k*gsize+n]) &&
-					   (newvalue 
-						== g[l*gsize+n]) &&
-					   (newvalue 
-						== g[m*gsize+n])) {
-		      					newcount++;
-					}
-				}
-			}
-		    }
-		}
-	    }
-	}
-    }
-
-    count = precount - oldcount + newcount;
-    return(count);
+    for (j=0;j<gsize;j++)
+        fprintf(logfp,"%d ",r[j]);
+    fclose(logfp);
+    free(r);
 }
-
-/*
- ***
- *** returns the number of monochromatic cliques in the graph presented to
- *** it
- ***
- *** graph is stored in row-major order
- *** only checks values above diagonal
- */
-
-void* CliqueCountLin(int *g,
-	     int gsize,void *edge_list)
-{
-    edge_list = FIFOResetEdge(edge_list);
-
-    int i;
-    int j;
-    int k;
-    int l;
-    int m;
-    int n;
-    int count=0;
-    int sgsize = 6;
-    
-    for(i=0;i < gsize-sgsize+1; i++)
-    {
-	for(j=i+1;j < gsize-sgsize+2; j++)
-        {
-	    for(k=j+1;k < gsize-sgsize+3; k++) 
-            { 
-		if((g[i*gsize+j] == g[i*gsize+k]) && 
-		   (g[i*gsize+j] == g[j*gsize+k]))
-		{
-		    for(l=k+1;l < gsize-sgsize+4; l++) 
-		    { 
-			if((g[i*gsize+j] == g[i*gsize+l]) && 
-			   (g[i*gsize+j] == g[j*gsize+l]) && 
-			   (g[i*gsize+j] == g[k*gsize+l]))
-			{
-			    for(m=l+1;m < gsize-sgsize+5; m++) 
-			    {
-				if((g[i*gsize+j] == g[i*gsize+m]) && 
-				   (g[i*gsize+j] == g[j*gsize+m]) &&
-				   (g[i*gsize+j] == g[k*gsize+m]) && 
-				   (g[i*gsize+j] == g[l*gsize+m])) {
-					for(n=m+1; n < gsize-sgsize+6; n++)
-					{
-						if((g[i*gsize+j]
-							== g[i*gsize+n]) &&
-						   (g[i*gsize+j] 
-							== g[j*gsize+n]) &&
-						   (g[i*gsize+j] 
-							== g[k*gsize+n]) &&
-						   (g[i*gsize+j] 
-							== g[l*gsize+n]) &&
-						   (g[i*gsize+j] 
-							== g[m*gsize+n])) {
-			      					count++;
-			      				if (!FIFOFindEdge(edge_list,i,j)) FIFOInsertEdge(edge_list,i,j);
-			      				if (!FIFOFindEdge(edge_list,i,k)) FIFOInsertEdge(edge_list,i,k);
-			      				if (!FIFOFindEdge(edge_list,i,l)) FIFOInsertEdge(edge_list,i,l);
-			      				if (!FIFOFindEdge(edge_list,i,m)) FIFOInsertEdge(edge_list,i,m);
-			      				if (!FIFOFindEdge(edge_list,i,n)) FIFOInsertEdge(edge_list,i,n);
-			      				if (!FIFOFindEdge(edge_list,j,k)) FIFOInsertEdge(edge_list,j,k);
-			      				if (!FIFOFindEdge(edge_list,j,l)) FIFOInsertEdge(edge_list,j,l);
-			      				if (!FIFOFindEdge(edge_list,j,m)) FIFOInsertEdge(edge_list,j,m);
-			      				if (!FIFOFindEdge(edge_list,j,n)) FIFOInsertEdge(edge_list,j,n);
-			      				if (!FIFOFindEdge(edge_list,k,l)) FIFOInsertEdge(edge_list,k,l);
-			      				if (!FIFOFindEdge(edge_list,k,m)) FIFOInsertEdge(edge_list,k,m);
-			      				if (!FIFOFindEdge(edge_list,k,n)) FIFOInsertEdge(edge_list,k,n);
-			      				if (!FIFOFindEdge(edge_list,l,m)) FIFOInsertEdge(edge_list,l,m);
-			      				if (!FIFOFindEdge(edge_list,l,n)) FIFOInsertEdge(edge_list,l,n);
-			      				if (!FIFOFindEdge(edge_list,m,n)) FIFOInsertEdge(edge_list,m,n);
-						}
-					}
-				}
-			    }
-			}
-		    }
-		}
-	    }
-         }
-     }
-    return(edge_list);
-}
-
-int* iterategenrow(int gsize,FILE* ofp){
-	// set a cap for the news, then we can have different range of combinations.
-	fprintf(ofp,"Start Iterating %d\n",gsize);
+int* iterategenrowmid(int gsize, FILE* ofp,int* mid,int startd){
+	//fprintf(ofp,"Start Mid %d\n",startd);
     int n = gsize/2;
     int k = n/2;
 
@@ -388,18 +260,16 @@ int* iterategenrow(int gsize,FILE* ofp){
     int i,j,d,ii;
     int same;
 
-    int *ret = (int *)malloc((n-k)*sizeof(int));
+    int *ret = (int *)malloc(startd*sizeof(int));
     int *row = (int *)malloc((n+1)*sizeof(int));
     int *wholerow = (int *)malloc(gsize*sizeof(int));
     int *newret;
-    for(i=2;i<n-k+2;i++){
-        ret[i-2] = i;
-    }
-    s = n-k;
+    s = 1;
+    for(i=0;i<startd;i++)
+        ret[i] = mid[i];
 
     int x,step,kk;
-    for (d=1;d<k;d++){
-        //newret = []
+    for (d=startd;d<k;d++){
         news = 0;
 
         for (i=0;i<s;i++){
@@ -447,6 +317,7 @@ int* iterategenrow(int gsize,FILE* ofp){
 	                	if (same >= 5)
 	                    	break;
 					}
+					
 					row[j] = 0;
 					wholerow[j] = row[j];
 					wholerow[gsize-j] = row[j];
@@ -478,15 +349,24 @@ int* iterategenrow(int gsize,FILE* ofp){
 	                	if (same >= 5)
 	                    	break;
 	                }
+	                step = 0;
+					for (kk=0;kk<j;kk++){
+						if (row[kk] == row[kk+1]){
+							step++;
+							if (step > 8)
+								break;
+						}
+						else
+							step = 1;			
+					}
 	                row[j] = 0;
-
-	                if (same >= 5)
+	                if (same >= 5 || step > 7)
 	                    continue;
 	            }
                 news++;
             }
-            if (news > 1000000)
-            	break;
+            //if (news > 10000000)
+            //	break;
         }
         newret = (int *)malloc((news)*(d+1)*sizeof(int));
         news = 0;
@@ -568,8 +448,18 @@ int* iterategenrow(int gsize,FILE* ofp){
 	                	if (same >= 5)
 	                    	break;
 	                }
+	                step = 0;
+					for (kk=0;kk<j;kk++){
+						if (row[kk] == row[kk+1]){
+							step++;
+							if (step > 8)
+								break;
+						}
+						else
+							step = 1;			
+					}
 	                row[j] = 0;
-	                if (same >= 5)
+	                if (same >= 5 || step > 7)
 	                    continue;
 				}
                 for (x=0;x<d;x++)
@@ -577,93 +467,200 @@ int* iterategenrow(int gsize,FILE* ofp){
                 newret[news*(d+1)+d] = j;
                 news++;
         	}
-        	if (news > 1000000)
-            	break;
+        	//if (news > 10000000)
+            //	break;
         }
         free(ret);
         ret = newret;
         s = news;
+        //printf("d:%d s:%d\n",d,s);
+        //fflush(stdout);
     }
-    fprintf(ofp,"(%d,%d) = %d\n",n,k,s);
+    /*fprintf(ofp,"(%d,%d) = %d\n",n,k,s);
     printf("(%d,%d) = %d\n",n,k,s);
     for(i=0;i<3;i++){
-    	for(j=0;j<k;j++){
-			fprintf(ofp,"%d ",ret[k*i+j]);
-			printf("%d ",ret[k*i+j]);
+    	for(j=0;j<d;j++){
+			fprintf(ofp,"%d ",ret[d*i+j]);
+			printf("%d ",ret[d*i+j]);
     	}
     	fprintf(ofp,"\n");
     	printf("\n");
-    }
+    }*/
     ret[0] = s;
     return ret;
 }
 
-void randomgenrow(int gsize){
-    int *r = (int *)malloc(gsize*sizeof(int));
-    int ones = gsize/2;
-    int zeros = gsize - ones;
-    int j = 0;
-    int k = 0;
-    int tmp;
-    int same;
-    int a = 1;
-    int step = 0;
-    int tt = 0;
-    FILE* logfp = fopen("../log/cycliclog.txt","a");
-    while (a == 1){
+int* iterategenrow(int gsize,FILE* ofp){
+	// set a cap for the news, then we can have different range of combinations.
+	fprintf(ofp,"Start Iterating %d\n",gsize);
+    int n = gsize/2;
+    int k = n/2;
 
-        r[0] = 0;
-        ones = gsize/2;
-        zeros = gsize - ones;
-        j = 1;
-        
-        while (j < gsize/2){
-            if (zeros == 0){
-                r[j] = 1;
-                r[gsize-j] = 1;
-                ones--;
-                if (j != gsize - j) ones--;
-                j++;
-                continue;
-            }else if (ones == 0){
-                r[j] = 0;
-                r[gsize-j] = 0;
-                if (j != gsize -j) zeros--;
-                j++;
-                continue;
-            }
-            
-            tmp = rand() % 2;
-            if (tmp == 0){
-                r[j] = 0;
-                r[gsize-j] = 0;
-                zeros--;
-            }else{
-                r[j] = 1;
-                r[gsize-j] = 1;
-                ones--;
-            }
-            j++;
-        }
-        for (step = 1;step < gsize/5;step++){
-            same = 1;
-            for (k=1;k<5;k++){
-                if (r[k*step] == r[(k+1)*step])
-                    same++;
-            }
-            if (same >= 5)
-                break;
-        }
-        if (same >= 5 && tt %100 == 0){
-            fprintf(logfp,"same: %d, step: %d tt: %d\n",same,step,tt);
-        }
-        tt++;
+    int s=0,news=0;
+    int f=0;
+
+    int end;
+
+    int i,j,d,ii;
+    int same;
+
+    
+    int *ret = (int *)malloc(1*sizeof(int));
+    int *row = (int *)malloc((n+1)*sizeof(int));
+    int *newret;
+    s = 0;
+    for(i=5;i<6;i++){
+        ret[s] = i;
+        s++;
     }
-    for (j=0;j<gsize;j++)
-        fprintf(logfp,"%d ",r[j]);
-    fclose(logfp);
-    free(r);
+
+    int x,step,kk;
+    for (d=1;d<k;d++){
+        //newret = []
+        news = 0;
+
+        for (i=0;i<s;i++){
+        	f = ret[i*d+d-1];
+            
+            for (x=0;x<n+1;x++)
+                row[x] = 0;
+        	for (x=0;x<d;x++)
+        		row[ret[i*d+x]] = 1;
+
+            same = 0;
+            for(j=f+1;j<n-k+2+d;j++){
+				row[j] = 1;
+				end = j/5;
+                for(step=1;step <= end;step++){
+                    same = 1;
+                    for(kk=1;kk<5;kk++)
+                        if (row[kk*step] == row[(kk+1)*step])
+                            same++;
+                    if (same >= 5)
+                    	break;
+
+                    same = 1;
+                    for (kk=1;kk<4;kk++)
+                    	if (row[kk*step] == row[(kk+1)*step])
+                            same++;
+                	if (same >= 4)
+                		for (ii=1;ii<=j-4*step;ii++)
+                			if (row[ii] == row[step] && row[ii+step] == row[step] && row[ii+2*step] == row[step]
+                				&& row[ii+3*step] == row[step] && row[ii+4*step] == row[step]){
+                				same = 5;
+                				break;
+                			}
+                	if (same >= 5)
+                    	break;
+                }
+                step = 0;
+				for (kk=0;kk<j;kk++){
+					if (row[kk] == row[kk+1]){
+						step++;
+						if (step > 8)
+							break;
+					}
+					else
+						step = 1;			
+				}
+                row[j] = 0;
+                if (same >= 5 || step > 7)
+                    continue;
+            
+                news++;
+            }
+
+            //if (news > 10000000)
+            //	break;
+        }
+        newret = (int *)malloc((news)*(d+1)*sizeof(int));
+        news = 0;
+
+        for (i=0;i<s;i++){
+        	f = ret[i*d+d-1];
+            
+            for (x=0;x<n+1;x++)
+                row[x] = 0;
+        	for (x=0;x<d;x++)
+        		row[ret[i*d+x]] = 1;
+            same = 0;
+
+            for(j=f+1;j<n-k+2+d;j++){
+            	
+				row[j] = 1;
+				
+				end = j/5;
+                for(step=1;step <= end;step++){
+
+                    same = 1;
+                    for(kk=1;kk<5;kk++)
+                        if (row[kk*step] == row[(kk+1)*step])
+                            same++;
+                    if (same >= 5)
+                    	break;
+
+                    same = 1;
+                    for (kk=1;kk<4;kk++)
+                    	if (row[kk*step] == row[(kk+1)*step])
+                            same++;
+                	if (same >= 4)
+                		for (ii=1;ii<=j-4*step;ii++)
+                			if (row[ii] == row[step] && row[ii+step] == row[step] && row[ii+2*step] == row[step]
+                				&& row[ii+3*step] == row[step] && row[ii+4*step] == row[step]){
+                				same = 5;
+                				break;
+                			}
+                	if (same >= 5)
+                    	break;
+                }
+                step = 0;
+				for (kk=0;kk<j;kk++){
+					if (row[kk] == row[kk+1]){
+						step++;
+						if (step > 8)
+							break;
+					}
+					else
+						step = 1;			
+				}
+                row[j] = 0;
+                if (same >= 5 || step > 7)
+                    continue;
+
+                for (x=0;x<d;x++)
+                	newret[news*(d+1)+x] = ret[i*d+x];
+                newret[news*(d+1)+d] = j;
+                news++;
+        	}
+        	//if (news > 10000000)
+            //	break;
+        }
+        free(ret);
+        ret = newret;
+        s = news;
+        fprintf(ofp,"d:%d s:%d\n",d,s);
+        if (s>10000000)
+        	break;
+        //fflush(stdout);
+    }
+    d++;
+    fprintf(ofp,"(%d,%d) = %d\n",n,k,s);
+    //printf("(%d,%d) = %d\n",n,k,s);
+    for(i=0;i<3;i++){
+    	for(j=0;j<d;j++){
+			fprintf(ofp,"%d ",ret[d*i+j]);
+			//printf("%d ",ret[d*i+j]);
+    	}
+    	fprintf(ofp,"\n");
+    	//printf("\n");
+    }
+    fflush(ofp);
+    ret[0] = s;
+    ret[1] = d;
+    return ret;
 }
+
+
 
 int main(int argc,char *argv[])
 {
@@ -682,8 +679,12 @@ int main(int argc,char *argv[])
     FILE *logfp = fopen("../log/cycliclog.txt","a");
 	FILE *ofp = fopen("../data/cyclicr66.txt","a");
     int* ret = iterategenrow(gsize,logfp);
+
+    int* finalret;
     int s = ret[0];
-    ret[0] = 2; 
+    int d = ret[1];
+    ret[0] = 5;
+    ret[1] = 6;
     if (1 == 2){
        return 1;
     }
@@ -691,45 +692,51 @@ int main(int argc,char *argv[])
 	/*
 	 * start with graph of size 8
 	 */
-	
 	g = (int *)malloc(gsize*gsize*sizeof(int));
 	if(g == NULL) {
 		exit(1);
 	}
-    /*
-	 * start out with random
-	 */
-	
-	int try;
-	for (try=0;try<s;try++){
-		memset(g,0,gsize*gsize*sizeof(int));
-		for (i=0;i<gsize;i++)
-			g[i] = 0;
-		
-		for (j=0;j<k;j++)
-			g[ret[try*k+j]] = 1;
-		for (i=gsize-1;i>n;i--)
-			g[i] = g[gsize-i];
+	int si,try,finals;
+	int* mid = (int *)malloc(d*sizeof(int));
 
-		for (i=1;i<gsize;i++){
-			for (j=i+1;j<gsize;j++)
-				g[i*gsize+j] = g[j-i];
-		}
+	for (si=0;si<s;si++){
+		for (i=0;i<d;i++)
+			mid[i] = ret[si*d+i];
 
-		count = CliqueCount(g,gsize);
-		if (try % 1000 == 0)
-			fprintf(logfp,"%d %d\n",try,count);
-		if (count == 0){
-			fprintf(logfp,"Eureka!  Counter-example found! %d\n",try);
-			PrintGraph(g,gsize,ofp);
-		}
-		if (s-try<10){
-			for (j=0;j<k;j++){
-				fprintf(logfp,"%d ",ret[try*k+j]);
+		finalret = iterategenrowmid(gsize,logfp,mid,d);
+		finals = finalret[0];
+		finalret[0] = 5;
+
+		if (si % 1000 == 0){
+			fprintf(logfp,"si/s: %d/%d\n",si,s);
+			fflush(logfp);
+		}	
+			
+		for (try=0;try<finals;try++){
+			memset(g,0,gsize*gsize*sizeof(int));
+			for (i=0;i<gsize;i++)
+				g[i] = 0;
+			
+			for (j=0;j<k;j++)
+				g[finalret[try*k+j]] = 1;
+			for (i=gsize-1;i>n;i--)
+				g[i] = g[gsize-i];
+
+			for (i=1;i<gsize;i++){
+				for (j=i+1;j<gsize;j++)
+					g[i*gsize+j] = g[j-i];
 			}
-			fprintf(logfp,"\n");
+
+			count = CliqueCount(g,gsize);
+			if (count == 0){
+				fprintf(logfp,"Eureka!  Counter-example found! si/s: %d/%d try/finals:%d/%d\n",si,s,try,finals);
+				fflush(logfp);
+				PrintGraph(g,gsize,ofp);
+			}
 		}
+		free(finalret);
 	}
+	free(ret);
 	fclose(ofp);
 	fclose(logfp);
 	return(0);
